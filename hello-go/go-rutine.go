c15f5b083e1bbb4014c31c5204a4c2841124fa0d
Rakib-28169-islam <rakibulislam28169@gmail.com>
2025-09-29 22:36:19 +0600

    goroutine exmaple

---
+++ hello-go/go-rutine.go
@@ -0,0 +1,52 @@
+package main
+import (
+	"fmt"
+	// "time"
+	"sync"
+)
+
+func printNumbers(msg string,wg *sync.WaitGroup){
+	defer wg.Done()
+	for i:=1;i<=5;i++{
+		fmt.Println(msg,":",i)
+		
+	}
+	// time.Sleep(1*time.Second)
+
+}
+func printMultiplicationTable(num int,wg *sync.WaitGroup){
+		defer wg.Done()
+
+	for i:=1;i<=10;i++{
+		fmt.Printf("%d x %d = %d\n",num,i,num*i)
+		
+	}
+	// time.Sleep(1*time.Second)
+}
+func printEvenOdd(num int,wg *sync.WaitGroup){
+	defer wg.Done()
+	if num%2==0{
+		fmt.Printf("%d is Even\n",num)
+	} else {
+		fmt.Printf("%d is Odd\n",num)
+	}
+	// time.Sleep(1*time.Second)
+} 
+func main(){
+	// printNumbers("Direct")
+    var wg sync.WaitGroup
+	
+	for i:=1;i<=3;i++{
+		wg.Add(3)
+		go printNumbers("Goroutine", &wg)
+		go printMultiplicationTable(i, &wg)
+		go printEvenOdd(i, &wg)
+		wg.Wait()
+		
+	}
+  
+	fmt.Print("\n\n")
+	wg.Wait()
+
+
+}
--- hello-go/hello.go
+++ hello-go/hello.go
@@ -1,6 +1,6 @@
 package main
 import "fmt"
 
-func main(){
-	fmt.Println("Hello, World!")
-}
+// func main(){
+// 	fmt.Println("Hello, World!")
+// }
