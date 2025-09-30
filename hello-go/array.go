677d78e885cea7e4ffac8d2ed9d770e0daebaf03
Rakib-28169-islam <rakibulislam28169@gmail.com>
2025-09-30 15:40:02 +0600

    working with array and slice

---
+++ hello-go/array.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+func arrayFunction(){
+		// Array
+	var arr = [3]int{1, 2, 3}
+	fmt.Println("Array:", arr)
+
+	// Slice
+	slice := []string{"apple", "banana", "cherry"}
+	slice = append(slice, "date")
+	fmt.Println("Slice:", slice)
+
+	// Map
+	m := make(map[string]int)
+	m["Alice"] = 25
+	m["Bob"] = 30
+	fmt.Println("Map:", m)
+
+	arrName := []int{1,2,3,4,5,6,7,8,9,10}
+	sliceArr:=arrName[0:len(arrName)/2]
+	fmt.Println("Int Slice:", sliceArr)
+	newArr:=arrName[:]
+	fmt.Println("New Array:", newArr)
+
+
+	original := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    
+    part1 := original[2:5]     // [2, 3, 4]
+    part2 := original[:3]      // [0, 1, 2] 
+    part3 := original[7:]      // [7, 8, 9] 
+    part4 := original[:]       // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
+
+    fmt.Println("Original:", original)
+    fmt.Println("part1 [2:5]:", part1)
+    fmt.Println("part2 [:3]:", part2)
+    fmt.Println("part3 [7:]:", part3)
+    fmt.Println("part4 [:]:", part4)
+
+	// Modifying the original slice
+	original[0] = -1000
+	fmt.Println("\nAfter modifying original:")
+     fmt.Println("Original:", original)
+    fmt.Println("part1 [2:5]:", part1)
+    fmt.Println("part2 [:3]:", part2)
+    fmt.Println("part3 [7:]:", part3)
+    fmt.Println("part4 [:]:", part4)
+
+
+	//slice copy without reference
+	fmt.Println("\nSlice Copy without reference:")
+	sliceA:=[]int{1,2,3,4,5}
+	sliceB:=make([]int,len(sliceA))
+	copy(sliceB,sliceA)
+	fmt.Println("Slice A:",sliceA)
+	fmt.Println("Slice B:",sliceB)
+	sliceA[0]=-100
+	fmt.Println("After modifying Slice A:")
+	fmt.Println("Slice A:",sliceA)
+	fmt.Println("Slice B:",sliceB)
+}
--- hello-go/main.go
+++ hello-go/main.go
@@ -1,11 +1,5 @@
 package main
 
-func main(){
-	//example code of array slice and map
-	 
-
-    
-
-	// ArraySliceMapExample()
-
+func main() {
+	arrayFunction()
 }
