89b65325de2c3b55e7bcf59c97d852b5c0141e75
Rakib-28169-islam <rakibulislam28169@gmail.com>
2025-10-01 20:37:07 +0600

    working with json body object

--- go-fiber/main.go
+++ go-fiber/main.go
@@ -1,48 +1,75 @@
 package main
 
 import (
-    "log"
-    "github.com/gofiber/fiber/v2"
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Age   int    `json:"age"`
+}
+
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, Fiber in VSCode!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, Fiber in VSCode!")
+	})
 
-    app.Get("/api/users", func(c *fiber.Ctx) error {
-        return c.JSON(fiber.Map{
-            "users": []string{"John", "Jane", "Bob"},
-        })
-    })
+	app.Get("/api/users", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"users": []string{"John", "Jane", "Bob"},
+		})
+	})
 
 	// Dynamic route with parameter
-	app.Put("/api/users/:id",func(c* fiber.Ctx)error{
+	app.Put("/api/users/:id", func(c *fiber.Ctx) error {
 		id := c.Params("id")
 		return c.JSON(fiber.Map{
 			"message": "User " + id + " created successfully",
 		})
 	})
 	//query parameter
-	app.Get("/api/users/search",func (c *fiber.Ctx)error{
-		query :=c.Query("q")
-		page := c.Query("page","1")//default value 1
+	app.Get("/api/users/search", func(c *fiber.Ctx) error {
+		query := c.Query("q")
+		page := c.Query("page", "1") //default value 1
 		return c.JSON(fiber.Map{
 			"query": query,
-			"page": page,
+			"page":  page,
 		})
 	})
 	// Multiple parameters
-    app.Get("/user/:id/post/:postId", func(c *fiber.Ctx) error {
-        id := c.Params("id")
-        postId := c.Params("postId")
-        return c.JSON(fiber.Map{
-            "userId": id,
-            "postId": postId,
-        })
-    })
+	app.Get("/user/:id/post/:postId", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		postId := c.Params("postId")
+		return c.JSON(fiber.Map{
+			"userId": id,
+			"postId": postId,
+		})
+	})
 
-    log.Fatal(app.Listen(":3000"))
+	app.Post("/api/users/create-user", func(c *fiber.Ctx) error {
+
+		var user User
+		if err := c.BodyParser(&user); err != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"status":  400,
+				"error":   "Invalid request body",
+				"details": err.Error(),
+			})
+		}
+		log.Printf("Received user: %+v\n", user)
+		fmt.Printf("%+v\n", user)
+		return c.JSON(fiber.Map{
+			"status":  "success",
+			"message": "User created successfully",
+			"user":    user,
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
