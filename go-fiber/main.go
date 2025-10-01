7439cbf3239e2a5f52f4246ca591a95bce2d6620
Rakib-28169-islam <rakibulislam28169@gmail.com>
2025-10-01 18:05:47 +0600

    working with routing

--- go-fiber/main.go
+++ go-fiber/main.go
@@ -18,12 +18,31 @@ func main() {
         })
     })
 
+	// Dynamic route with parameter
 	app.Put("/api/users/:id",func(c* fiber.Ctx)error{
 		id := c.Params("id")
 		return c.JSON(fiber.Map{
 			"message": "User " + id + " created successfully",
 		})
 	})
+	//query parameter
+	app.Get("/api/users/search",func (c *fiber.Ctx)error{
+		query :=c.Query("q")
+		page := c.Query("page","1")//default value 1
+		return c.JSON(fiber.Map{
+			"query": query,
+			"page": page,
+		})
+	})
+	// Multiple parameters
+    app.Get("/user/:id/post/:postId", func(c *fiber.Ctx) error {
+        id := c.Params("id")
+        postId := c.Params("postId")
+        return c.JSON(fiber.Map{
+            "userId": id,
+            "postId": postId,
+        })
+    })
 
     log.Fatal(app.Listen(":3000"))
 }
